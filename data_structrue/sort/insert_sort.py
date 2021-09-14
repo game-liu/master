8805d5888c2b131c378793c34d89c8b75cbd4c29
liu <18342284986@163.com>
2021-09-14 23:55:46 +0800

    improvement(插入排序)

--- data_structrue/sort/insert_sort.py
+++ data_structrue/sort/insert_sort.py
@@ -1,10 +1,10 @@
 # 直接插入排序
 def insert_sort(lists):
     n = len(lists)
-    for i in range(1, n):
-        key = lists[i]
-        j = i - 1
-        while j >= 0 and lists[j] > key:
+    for i in range(1, n):  # 从第二个元素开始排，默认第一个元素已经是排好的
+        key = lists[i]  # 基数为当前元素
+        j = i - 1  # 从后开始扫描该元素之前的元素序列
+        while j >= 0 and lists[j] > key:  # 如果当前元素比基数大的话，调换位置
             lists[j + 1] = lists[j]
             lists[j] = key
             j -= 1
